b0869c085654d6882aa6cfba60efb14d57b309a2
juntae710 <pjt010914@gmail.com>
2023-10-26 15:23:22 +0900

    mysql NO.9,10

--- SQL/baemin_mysql_db.sql
+++ SQL/baemin_mysql_db.sql
@@ -1,3 +1,5 @@
+use baemin_db;
+
 -- 01 
 
 -- 02
@@ -15,8 +17,29 @@
 -- 08
 
 -- 09
+CREATE TABLE review_tbl (
+    reviewCode INT AUTO_INCREMENT PRIMARY KEY,
+    menuCode VARCHAR(20),
+    userCode VARCHAR(10),
+    storeCode VARCHAR(13),
+    reviewImage VARCHAR(20) NOT NULL,
+    reviewDate DATE NOT NULL,
+    reviewRating INT NOT NULL,
+    reviewContent VARCHAR(300) NOT NULL,
+   FOREIGN KEY (menuCode) REFERENCES menu_tbl(menuCode),
+   FOREIGN KEY (userCode) REFERENCES user_tbl(userCode),
+   FOREIGN KEY (storeCode) REFERENCES store_tbl(storeCode)
+) AUTO_INCREMENT=90000;
+
 
 -- 10
+CREATE TABLE answer_tbl (
+    answerCode VARCHAR(20) NOT NULL PRIMARY KEY,
+    reviewCode VARCHAR(20),
+    answerDate DATE NOT NULL,
+    answerContent VARCHAR(300) NOT NULL,
+    FOREIGN KEY (reviewCode) REFERENCES review_tbl(reviewCode)
+);
 
 -- 11
 
