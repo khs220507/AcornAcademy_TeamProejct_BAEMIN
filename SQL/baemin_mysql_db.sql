1ae56fa96f63b3e1ad7d7f7bad18534decf79438
KIM.M.J <keyti0729@gmail.com>
2023-10-26 16:49:02 +0900

    sql 민지

--- SQL/baemin_mysql_db.sql
+++ SQL/baemin_mysql_db.sql
@@ -1,8 +1,35 @@
 use baemin_db;
 
--- 01 
+-- 01 회원 user_tbl
+create table user_tbl(
+    userCode int auto_increment primary key,
+    userId varchar(20) not null,
+    userPw varchar(30) not null,
+    userName varchar(20) not null,
+    userNickname  varchar(20) not null,
+    userPhone varchar(50) not null,
+    userAddress varchar(150) not null,
+    userEmail varchar(50) not null,
+    userBirth varchar(20) not null,
+    userGender tinyint(1) not null, -- 0:남자, 1:여자
+    userStatus tinyint(1) not null -- 0:회원탈퇴, 1:정상회원
+) auto_increment=10000;
 
--- 02
+select * from user_tbl;
+
+-- 02 사장 seller_tbl
+create table seller_tbl(
+	sellerRegCode int auto_increment primary key,
+    sellerId varchar(20) not null,
+    sellerPw varchar(30) not null,
+    sellerName varchar(20) not null,
+    sellerPhone varchar(50) not null,
+    sellerEmail varchar(50) not null,
+    sellerBirth varchar(20) not null,
+	sellerGender tinyint(1) not null, -- 0:남자, 1:여자
+    sellerStatus  tinyint(1) not null -- 0:회원탈퇴, 1:정상회원
+)auto_increment=20000;
+
 
 -- 03
 
